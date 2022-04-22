d491823e83a58b0b33bd932f4cb59fbf33fbf0a8
Ratnadip Bhorge <87796826+RatnadipBhorge@users.noreply.github.com>
2022-04-22 11:01:53 +0530

    Add files via upload

---
+++ ATT.bat
@@ -0,0 +1,11 @@
+@echo off
+
+
+
+call C:\Users\Ratnadip.bhorge\Anaconda3\Scripts\activate.bat C:\Users\Ratnadip.bhorge\Anaconda3
+
+
+
+papermill "C:\Users\Ratnadip.bhorge\Documents\Python Scripts\Daily DE attendance.ipynb" "C:\Users\Ratnadip.bhorge\Documents\Python Scripts\scheduletrial\dummytrialpapermill.ipynb"
+
+pause
